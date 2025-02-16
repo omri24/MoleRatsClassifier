1e40313fcfdb4e9e4dd132fe89f59651de9f7de8
Omri <omrilip1@gmail.com>
2025-02-16 16:58:25 +0200

    add a plot of the embedding space

--- plot_embedding_space.py
+++ plot_embedding_space.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 
 
-def plot_csv_rows(csv_path_1, csv_path_2, amount_to_plot=1):
+def plot_csv_rows(csv_path_1, csv_path_2, amount_to_plot=1, export=False):
     # Load the CSV files into DataFrames
     df1 = pd.read_csv(csv_path_1, header=None)
     df2 = pd.read_csv(csv_path_2, header=None)
@@ -16,11 +16,13 @@ def plot_csv_rows(csv_path_1, csv_path_2, amount_to_plot=1):
     for i in range(df2.shape[0]):  # Iterate over rows
       if i < amount_to_plot:
         plt.plot(df2.columns, df2.iloc[i], "r", label="noise")
-    plt.title("Embedding space of BMR and noise")
-    plt.xlabel("Columns")
-    plt.ylabel("Values")
+    plt.title("Embedding space")
+    plt.xlabel("Index in embedding space")
+    plt.ylabel("Value")
     plt.legend()
     plt.grid()
+    if export:
+        plt.savefig("Embedding_space_plot.png", dpi=300, bbox_inches="tight")
     plt.show()
 
 path_to_embeddings = "Embeddings"
@@ -28,4 +30,4 @@ path_to_embeddings = "Embeddings"
 csv_file_1 = os.path.join(path_to_embeddings, "R1S1_embeddings_ch0_label1.csv")   # Make sure this file is BMR
 csv_file_2 = os.path.join(path_to_embeddings, "R1S1_embeddings_ch0_label2.csv")   # Make sure this file is noise
 
-plot_csv_rows(csv_file_1, csv_file_2, 1)
+plot_csv_rows(csv_file_1, csv_file_2, 1, True)
